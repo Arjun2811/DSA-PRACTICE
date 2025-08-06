89c03aacc45922a6ebd64b19246569b9357a1a1a
Arjun2811 <arjun.as.2207@gmail.com>
2025-08-05 19:59:33 -0500

    Added graph question

---
+++ Graphs2/src/Threecycle/Cycle.java
@@ -0,0 +1,81 @@
+package Threecycle;
+import java.util.Scanner;
+public class Cycle {
+
+
+
+
+
+public static int Cycle(boolean[][]arr,int fv,boolean visited[],int originalfv,int n)
+{
+
+visited[fv]=true;
+
+if(n==3)
+{int ans=0;
+    if(arr[fv][originalfv]==true)
+    {
+ ans=1;
+
+    }
+    return ans;
+}
+int i=0;
+int finalans=0;
+while(i<arr.length)
+{
+    if(arr[fv][i]==true&&visited[i]==false)
+    {
+         int ans=Cycle(arr,i,visited,originalfv,n+1);
+         finalans+=ans;
+    }
+    i++;
+}
+return finalans;
+
+
+
+
+}
+
+
+
+
+public static int helper(boolean[][]arr)
+{
+
+
+int i=0;
+int count=0;
+
+while(i<arr.length)
+{
+    boolean visited[]=new boolean[arr.length];
+    visited[i]=true;
+ count+=Cycle(arr,i,visited,i,1);
+    i++;
+}
+return count;
+}
+
+
+    public static void main(String[]args)
+    {Scanner s = new Scanner(System.in);
+        
+        int v=s.nextInt();
+        int e=s.nextInt();
+        boolean arr[][]=new boolean[v][v];
+        int i=0;
+        while(i<e)
+        {
+            int fv=s.nextInt();
+            int lv=s.nextInt();
+        arr[fv][lv]=true;
+        arr[lv][fv]=true;
+        i++;
+        }
+        int count=helper(arr);
+        System.out.println(count/v);
+
+    }
+}
